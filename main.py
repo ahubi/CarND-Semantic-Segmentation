c75c29d8591859594399e1a1c43014ad563055cc
artur <babalasoft@gmail.com>
2018-01-30 01:38:47 +0100

    implement load, layers

--- main.py
+++ main.py
@@ -32,8 +32,17 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +56,27 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same',
+                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding='same',
+                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    #print(tf.Print(output, [tf.shape(output)]))
+    conv4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same',
+                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    input = tf.add(conv4, output)
+
+    output = tf.layers.conv2d_transpose(input, num_classes, 4, 2, padding='same',
+                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    conv3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same',
+                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    input = tf.add(conv3, output)
+    output = tf.layers.conv2d_transpose(input, num_classes, 16, 8, padding='same',
+                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    return output
 tests.test_layers(layers)
 
 
